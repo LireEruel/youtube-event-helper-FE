5f6c2a4e1e8feb3835b7372511c3d781a8b38e1e
LireEruel <jsh001505@gmail.com>
2023-01-28 14:44:00 +0900

    initials

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { Button, Space, DatePicker, Card } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
+export default function Home() {
+  const onChange = () => {};
+  return (
+    <div style={{ padding: 100 }}>
+      <Space direction="vertical">
+        <Button type="primary">Primary Button</Button>
+        <Button type="ghost">Ghost Button</Button>
+        <DatePicker onChange={onChange} />
+        <SearchOutlined style={{fontSize: 60, color: 'green'}} />
+        <Card>
+          <p>Card content</p>
+        </Card>
+      </Space>
+    </div>
+  );
+}
